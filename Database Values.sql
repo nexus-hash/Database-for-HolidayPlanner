f679cb7d0f8f4e270ef9287bd473651b949bc0fb
soumya17052002 <56285471+soumya17052002@users.noreply.github.com>
2020-11-06 19:27:43 +0530

    Added insert values commands

---
+++ Database Values.sql
@@ -0,0 +1,218 @@
+insert into flight values ('6E789','Delhi','Mumbai','indigo','06:30:00','08:25:00','6780');
+insert into flight values ('6E790','Mumbai','Delhi','indigo','12:13:00','16:20:00','6799');
+insert into flight values ('6E734','Delhi','Mumbai','indigo','16:12:00','17:59:00','7540');
+insert into flight values ('6E735','Mumbai','Delhi','indigo','06:00:00','08:12:00','7742');
+INSERT INTO flight(flightnumber, departure, arrival, airlinename, departuretime, arrivaltime, flightfare) VALUES ('F0011', 'Delhi', 'Jaipur', 'Indian Airine', '16:00:00', '17:40:00', '2000');
+INSERT INTO flight(flightnumber, departure, arrival, airlinename, departuretime, arrivaltime, flightfare) VALUES ('F0012', 'Delhi', 'Jaipur', 'Fast Airine', '08:00:00', '09:40:00', '2500');
+INSERT INTO flight(flightnumber, departure, arrival, airlinename, departuretime, arrivaltime, flightfare) VALUES ('F0013', 'Delhi', 'Jaipur', 'Now Airine', '20:00:00', '21:40:00', '2200');
+INSERT INTO flight(flightnumber, departure, arrival, airlinename, departuretime, arrivaltime, flightfare) VALUES ('F0014', 'Delhi', 'Jaipur', 'My Airine', '12:00:00', '13:40:00', '2200');
+INSERT INTO flight(flightnumber, departure, arrival, airlinename, departuretime, arrivaltime, flightfare) VALUES ('F0021', 'Chennai', 'Mumbai', 'Wow Airine', '11:00:00', '13:00:00', '6000');
+INSERT INTO flight(flightnumber, departure, arrival, airlinename, departuretime, arrivaltime, flightfare) VALUES ('F0022', 'Chennai', 'Mumbai', 'Now Airine', '07:00:00', '09:00:00', '4500');
+INSERT INTO flight(flightnumber, departure, arrival, airlinename, departuretime, arrivaltime, flightfare) VALUES ('F0023', 'Chennai', 'Mumbai', 'Now Airine', '20:00:00', '22:00:00', '3200');
+INSERT INTO flight(flightnumber, departure, arrival, airlinename, departuretime, arrivaltime, flightfare) VALUES ('F0024', 'Chennai', 'Mumbai', 'My Airine', '12:00:00', '14:00:00', '4200');
+
+insert into flightavailability values ('6E789','2020-11-05',186);
+insert into flightavailability values ('6E789','2020-11-06',186);
+insert into flightavailability values ('6E789','2020-11-07',186);
+insert into flightavailability values ('6E789','2020-11-08',186);
+insert into flightavailability values ('6E789','2020-11-09',186);
+insert into flightavailability values ('6E790','2020-11-05',186);
+insert into flightavailability values ('6E790','2020-11-06',186);
+insert into flightavailability values ('6E790','2020-11-07',186);
+insert into flightavailability values ('6E790','2020-11-08',186);
+insert into flightavailability values ('6E790','2020-11-09',186);
+
+insert into flightavailability values ('6E734','2020-11-05',186);
+insert into flightavailability values ('6E734','2020-11-06',186);
+insert into flightavailability values ('6E734','2020-11-07',186);
+insert into flightavailability values ('6E734','2020-11-08',186);
+insert into flightavailability values ('6E734','2020-11-09',186);
+insert into flightavailability values ('6E735','2020-11-05',186);
+insert into flightavailability values ('6E735','2020-11-06',186);
+insert into flightavailability values ('6E735','2020-11-07',186);
+insert into flightavailability values ('6E735','2020-11-08',186);
+insert into flightavailability values ('6E735','2020-11-09',186);
+
+insert into flightavailability values ('F0011','2020-11-06',186);
+insert into flightavailability values ('F0011','2020-11-07',186);
+insert into flightavailability values ('F0011','2020-11-08',186);
+insert into flightavailability values ('F0011','2020-11-09',186);
+insert into flightavailability values ('F0011','2020-11-10',186);
+insert into flightavailability values ('F0011','2020-11-11',186);
+insert into flightavailability values ('F0011','2020-11-12',186);
+insert into flightavailability values ('F0012','2020-11-06',186);
+insert into flightavailability values ('F0012','2020-11-07',186);
+insert into flightavailability values ('F0012','2020-11-08',186);
+insert into flightavailability values ('F0012','2020-11-09',186);
+insert into flightavailability values ('F0012','2020-11-10',186);
+insert into flightavailability values ('F0012','2020-11-11',186);
+insert into flightavailability values ('F0012','2020-11-12',186);
+
+insert into flightavailability values ('F0013','2020-11-06',186);
+insert into flightavailability values ('F0013','2020-11-07',186);
+insert into flightavailability values ('F0013','2020-11-08',186);
+insert into flightavailability values ('F0013','2020-11-09',186);
+insert into flightavailability values ('F0013','2020-11-10',186);
+insert into flightavailability values ('F0013','2020-11-11',186);
+insert into flightavailability values ('F0013','2020-11-12',186);
+insert into flightavailability values ('F0013','2020-11-13',186);
+insert into flightavailability values ('F0013','2020-11-14',186);
+insert into flightavailability values ('F0013','2020-11-15',186);
+insert into flightavailability values ('F0014','2020-11-06',186);
+insert into flightavailability values ('F0014','2020-11-07',186);
+insert into flightavailability values ('F0014','2020-11-08',186);
+insert into flightavailability values ('F0014','2020-11-09',186);
+insert into flightavailability values ('F0014','2020-11-10',186);
+insert into flightavailability values ('F0014','2020-11-11',186);
+insert into flightavailability values ('F0014','2020-11-12',186);
+insert into flightavailability values ('F0014','2020-11-13',186);
+
+insert into flightavailability values ('F0021','2020-11-06',186);
+insert into flightavailability values ('F0021','2020-11-07',186);
+insert into flightavailability values ('F0021','2020-11-08',186);
+insert into flightavailability values ('F0021','2020-11-09',186);
+insert into flightavailability values ('F0021','2020-11-10',186);
+insert into flightavailability values ('F0021','2020-11-11',186);
+insert into flightavailability values ('F0021','2020-11-12',186);
+insert into flightavailability values ('F0021','2020-11-13',186);
+insert into flightavailability values ('F0021','2020-11-14',186);
+insert into flightavailability values ('F0021','2020-11-15',186);
+insert into flightavailability values ('F0022','2020-11-06',186);
+insert into flightavailability values ('F0022','2020-11-07',186);
+insert into flightavailability values ('F0022','2020-11-08',186);
+insert into flightavailability values ('F0022','2020-11-09',186);
+insert into flightavailability values ('F0022','2020-11-10',186);
+insert into flightavailability values ('F0022','2020-11-11',186);
+insert into flightavailability values ('F0022','2020-11-12',186);
+insert into flightavailability values ('F0022','2020-11-13',186);
+insert into flightavailability values ('F0022','2020-11-14',186);
+insert into flightavailability values ('F0022','2020-11-15',186);
+
+insert into flightavailability values ('F0023','2020-11-06',186);
+insert into flightavailability values ('F0023','2020-11-07',186);
+insert into flightavailability values ('F0023','2020-11-08',186);
+insert into flightavailability values ('F0023','2020-11-09',186);
+insert into flightavailability values ('F0023','2020-11-10',186);
+insert into flightavailability values ('F0023','2020-11-11',186);
+insert into flightavailability values ('F0023','2020-11-12',186);
+insert into flightavailability values ('F0023','2020-11-13',186);
+insert into flightavailability values ('F0023','2020-11-14',186);
+insert into flightavailability values ('F0023','2020-11-15',186);
+insert into flightavailability values ('F0024','2020-11-06',186);
+insert into flightavailability values ('F0024','2020-11-07',186);
+insert into flightavailability values ('F0024','2020-11-08',186);
+insert into flightavailability values ('F0024','2020-11-09',186);
+insert into flightavailability values ('F0024','2020-11-10',186);
+insert into flightavailability values ('F0024','2020-11-11',186);
+insert into flightavailability values ('F0024','2020-11-12',186);
+insert into flightavailability values ('F0024','2020-11-13',186);
+insert into flightavailability values ('F0024','2020-11-14',186);
+insert into flightavailability values ('F0024','2020-11-15',186);
+
+
+
+
+insert into packagecontainsflights values ('pk333','6E789');
+insert into packagecontainsflights values ('pk333','6E790');
+insert into packagecontainsflights values ('P0010','F0021');
+
+
+insert into tour values ('T0001','Temple Tour','vsdvvdssxc','Walkeswar Temple','Shri Siddhi Vinayak Ganpati Mandir','temples',1,23400);
+insert into tour values ('T0002','Water Park','wadawfaef','Water Kingdom','Water Kingdom','Water Kingdom',1,3500);
+insert into tour values ('T0003','Mumbai`s Beaches','Enjoy the Aksa beach in Mumbai with some street food from the food stalls and some surprise from our side','From Hotel','Aksa Beach','Aksa Beach',1,2900);
+INSERT INTO tour(tourid, tourname, tourdescription, placefrom, placestobe, duration, tourfare) VALUES ('T0011', 'Jaipur City', 'A few Local Places in the Main City', 'City Center', 'Hawa Mahal, Famous Food', 2, 1200);
+INSERT INTO tour(tourid, tourname, tourdescription, placefrom, placestobe, duration, tourfare) VALUES ('T0012', 'Jaipur City', 'A Multiple Places in the City', 'City Center', 'Hawa Mahal, Famous Food, Lake City', 3, 1500);
+INSERT INTO tour(tourid, tourname, tourdescription, placefrom, placestobe, duration, tourfare) VALUES ('T0013', 'Jaipur City', 'A Few Places in and Around Jaipur', 'City Center', 'Hawa Mahal, Famous Food, Lake City, Ajmer Peth, plus 1 Mahal', 4, 2000);
+INSERT INTO tour(tourid, tourname, tourdescription, placefrom, placestobe, duration, tourfare) VALUES ('T0014', 'Jaipur City', 'Multiple Places in and Around Jaipur', 'City Center', 'Hawa Mahal, Famous Food, Lake City, Ajmer Peth, plus 3 Mahal', 6, 2300);
+
+
+insert into packagecontainstours values ('pk333','T0001');
+insert into packagecontainstours values ('pk333','T0003');
+insert into packagecontainstours values ('P0010','T0011');
+insert into packagecontainstours values ('P0010','T0012');
+insert into packagecontainstours values ('P0010','T0013');
+insert into packagecontainstours values ('P0010','T0014');
+
+-- insert into cab values();
+
+select  tourid, tourname,placefrom,duration,tourfare from tour;
+
+insert into cab values ('C0001','Sedan','19.093026, 72.856547','3500','MH 02 AE 9023',true,'Toyota Etios Cross','9874459933');
+insert into cab values ('C0002','SUV','19.093026, 72.856547','3800','MH 02 AF 9023',true,'Hundai Venue','9873343344');
+insert into cab values ('C0003','Sedan','19.093026, 72.856547','3500','MH 02 AC 9023',true,'Toyota Etios Cross','9872112233');
+INSERT INTO cab(cabid, cabtype, cablocation, cabfare, cabvehicleno,availability, cabmodel, driverno) VALUES ('C0011', 'Sedan', 'Jaipur City', '500', 'IND001', true, 'Toyota Etios Cross', '1256875');
+INSERT INTO cab(cabid, cabtype, cablocation, cabfare, cabvehicleno,availability, cabmodel, driverno) VALUES ('C0012', 'Sedan', 'Jaipur City', '500', 'IND101',true, 'Toyota Etios Cross', '125256875');
+INSERT INTO cab(cabid, cabtype, cablocation, cabfare, cabvehicleno,availability, cabmodel, driverno) VALUES ('C0013', 'SUV', 'Jaipur City', '800', 'IND011', true, 'Hundai Venue', '54321811');
+INSERT INTO cab(cabid, cabtype, cablocation, cabfare, cabvehicleno,availability, cabmodel, driverno) VALUES ('C0014', 'Mini', 'Jaipur City', '350', 'IND211', true, 'Maruti i10', '64295132');
+
+insert into tourcontainscab values ('T0001','C0001');
+insert into tourcontainscab values ('T0001','C0002');
+insert into tourcontainscab values ('T0001','C0003');
+insert into tourcontainscab values ('T0002','C0002');
+
+
+
+select cabid,cabtype,cabfare,availability,cabmodel from cab where cab.cabid in (Select tourcontainscab.cabid from tourcontainscab where tourid='T0001');
+
+insert into hotel values ('H0001','Rivea','19.092797, 72.853869','A112112');
+INSERT INTO hotel(hotelid, hotelname, hotellocation, areacode) VALUES ('H0011', 'Delight Inn', '26.9124N,75.7873E', 'JAI0141');
+INSERT INTO hotel(hotelid, hotelname, hotellocation, areacode) VALUES ('H0012', 'Mahal Hotel', '26.9104N,75.7803E', 'JAI0141');
+
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail) values ('H0001','2200','2',32,'2020-11-05');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','3200','3',12,'2020-11-05');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','1600','1',44,'2020-11-05');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','2200','2',31,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','3200','3',08,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','1600','1',81,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','2200','2',23,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','3200','3',14,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','1600','1',22,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','2200','2',03,'2020-11-08');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','3200','3',13,'2020-11-08');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0001','1600','1',0,'2020-11-08');
+
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail) values ('H0011','3200','2',32,'2020-11-09');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','4200','3',12,'2020-11-09');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','2300','1',44,'2020-11-09');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','3200','2',31,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','4200','3',08,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','2300','1',81,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','3200','2',23,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','4200','3',14,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','2300','1',22,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','3200','2',03,'2020-11-08');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','4200','3',13,'2020-11-08');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0011','2300','2',0,'2020-11-08');
+
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail) values ('H0012','3400','2',32,'2020-11-09');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','4900','3',12,'2020-11-09');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','2900','1',44,'2020-11-09');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','3400','2',31,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','4900','3',08,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','2900','1',81,'2020-11-06');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','3400','2',23,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','4900','3',14,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','2900','1',22,'2020-11-07');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','3400','2',03,'2020-11-08');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','4900','3',13,'2020-11-08');
+insert into hoteldetailed(hotelid, hotelfare, noofbeds, availability, dateavail)  values ('H0012','2900','2',0,'2020-11-08');
+
+
+
+insert into statedetails values ('A112112','Mumbai','City of Dreams','Maharastra','India');
+INSERT INTO statedetails(areacode, cityname, citydescription,statename,country) VALUES ('JAI0141', 'Jaipur', 'North Indian Delight', 'Rajasthan', 'India');
+INSERT INTO statedetails(areacode, cityname, citydescription,statename,country) VALUES ('DEL0011', 'Delhi', 'Indian Capital', 'Delhi', 'India');
+
+insert into packagecontainshotel values ('pk333','H0001');
+
+
+insert into tourguide values ('G0001','Lokesh Parikh','M','English,Hindi,Marathi,Japanese','8992032300',400,'lokesh.parikh@gmail.com');
+INSERT INTO tourguide(guideid, guidename, gender, knownlanguage, phonenumber, feeamount, emailid) VALUES ('G0020', 'Anubhav', 'M', 'English, Hindi, Marathi', '67895432', 550, 'anubhav@holiday.com');
+INSERT INTO tourguide(guideid, guidename, gender, knownlanguage, phonenumber, feeamount, emailid) VALUES ('G0021', 'Vijay', 'M', 'English, Hindi, Marathi, Gujrati', '12486345', 575, 'vijay@holiday.com');
+INSERT INTO tourguide(guideid, guidename, gender, knownlanguage, phonenumber, feeamount, emailid) VALUES ('G0022', 'Jaya', 'F', 'English, Hindi', '49873153', 400, 'jaya@holiday.com');
+INSERT INTO tourguide(guideid, guidename, gender, knownlanguage, phonenumber, feeamount, emailid) VALUES ('G0010', 'Vikrant', 'M', 'English, Hindi', '54658213', 250, 'vikrant@holiday.com');
+INSERT INTO tourguide(guideid, guidename, gender, knownlanguage, phonenumber, feeamount, emailid) VALUES ('G0011', 'Vimal', 'M', 'English, Hindi', '12353498', 275, 'vimal@holiday.com');
+INSERT INTO tourguide(guideid, guidename, gender, knownlanguage, phonenumber, feeamount, emailid) VALUES ('G0012', 'Jaishree', 'F', 'English, Hindi, Marwari', '49853256', 300, 'jaishree@holiday.com');
+
+insert into tourcontainsguide
+values ('T0001','G0001');
